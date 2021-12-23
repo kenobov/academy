f20c525af23d71a8d7f90f3492172c948a047a70
Kenobov <daelin18@yandex.by>
2021-12-23 18:34:39 +0300

    br2js fix

--- br2jsx/src/Br2jsComponent/Br2jsComponent.js
+++ br2jsx/src/Br2jsComponent/Br2jsComponent.js
@@ -2,11 +2,15 @@ import React from 'react';
 
 const Br2jsComponent = props => {
 
+    let renderHTML = [], elements = props.str.split(/<br\s?\/?>/gi);
+    elements.forEach((line, index) => {
+        renderHTML.push(line);
+        if(index !== elements.length-1) renderHTML.push(<br key={index}/>);
+    });
+
     return (
-        <div style={{display: "inline-block", margin: "20px", padding: "50px", border: '2px solid #000'}}>
-            {
-                props.str.split(/<br\s?\/?>/gi).map(line => <>{line}<br/></>)
-            }
+        <div style={{display: "inline-block", margin: "20px", padding: "50px", border: '2px solid #000', textAlign: 'center'}}>
+            { renderHTML }
         </div>
     )
 
